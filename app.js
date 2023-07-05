efe79a4eba70886d124c28c89c77fbb6639908da
FrancisCKY <a231685600@gmail.com>
2023-07-05 12:22:25 +0800

    build user express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express")
+const { engine } = require("express-handlebars")
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
 
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
   res.redirect('/movies')
 })
 
 app.get('/movies', (req, res) => {
-  res.send('Listing movies')
+  res.render('home')
 })
 
 app.get('/movies/:id', (req, res) => {
